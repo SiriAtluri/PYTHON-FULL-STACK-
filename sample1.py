b96dbadbcb3c45a2dd162b71851f528cb6474324
Siri Atluri <siriatluri2005@gmail.com>
2026-06-25 17:39:19 +0530

    Add files via upload

---
+++ sample1.py
@@ -0,0 +1,17 @@
+Python 3.14.4 (tags/v3.14.4:23116f9, Apr  7 2026, 14:10:54) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> print(10+90)
+100
+>>> print(7+3)
+10
+>>> print("python")
+python
+>>> Print(3-2)
+Traceback (most recent call last):
+  File "<pyshell#3>", line 1, in <module>
+    Print(3-2)
+NameError: name 'Print' is not defined. Did you mean: 'print'?
+>>> print(3-2)
+1
+print(8+2)
+
