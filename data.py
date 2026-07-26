14a238bcbbe2eee60ef9a1816017e46e25502691
Siri Atluri <siriatluri2005@gmail.com>
2026-07-26 15:20:08 +0530

    Add files via upload

---
+++ data.py
@@ -0,0 +1,13 @@
+'''import mymodule
+mymodule.greetings("siri")'''
+
+#import mymodule
+
+
+#import mymodule
+
+'''import mymodule
+b=mymodule.details["idnos"]
+c=mymodule.details["names"]
+print(b)
+print(c)'''
